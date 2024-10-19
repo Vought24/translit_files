493dad1680524389a7a740316c8ad10db86b6e90
Volodimir <zanfir.ukr@gmail.com>
2024-10-19 21:22:15 +0200

    Update transli.py

--- transli.py
+++ transli.py
@@ -6,12 +6,18 @@ from transliterate import translit
 root = Tk()
 root.title('Транслитерация песен')
 root.geometry('800x800')
-root.resizable(False, False)
+root.resizable(False, True)
 
 
-show_result = Text(root, height=30, width=80)
-show_result.place(x=30, y=120)
-show_result.config(bg='beige', font=('Arial', 13), borderwidth=2, cursor='ibeam')
+show_result = Text(root, height=30, width=79)
+show_result.place(x=40, y=150)
+show_result.config(bg='beige', 
+                   font=('Arial', 13), 
+                   borderwidth=2, 
+                   cursor='ibeam',
+                   highlightcolor="red",
+                   #borderwidth=4,
+                   )
 
 def show_process(msg: str) -> None:
     show_result.insert(END, msg + '\n')
@@ -55,8 +61,19 @@ def dir_choice():
     if directory:
         rename_music_files(directory)
         
+clue_label = Label(text="Выберите папку с файлами которые вы хотите транслитерировать...",  font=('Arial', 14))
+clue_label.place(x=40, y=10)
 
-run_btn = ttk.Button(text='Choose directory', command=dir_choice)
-run_btn.place(x=50, y=50)
-
+label_style = ttk.Style()
+label_style.configure('button_style.TButton',          # имя стиля
+                    font="helvetica 14",    # шрифт
+                    foreground="#004D40",   # цвет текста
+                    padding=10,             # отступы
+                    background="#B2DFDB",
+                    relief="solid",
+                    cursor='hand1',
+                    width="63"
+                    )
+run_btn = ttk.Button(text='Choose directory', command=dir_choice,  style="button_style.TButton")
+run_btn.place(x=40, y=60)
 root.mainloop()
